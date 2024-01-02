db6aabb624ff6f862b360f882e7a37abe7bb0c5a
baffa-m <ahmadalmustapha4@gmail.com>
2024-01-02 23:17:44 +0100

    modified users table

--- database/migrations/2024_01_02_221650_create_users_table.php
+++ database/migrations/2024_01_02_221650_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('is_admin')->default(0);
+            $table->foreignId('state_id')->references('id')->on('states')->onDelete('cascade');
             $table->rememberToken();
             $table->timestamps();
         });
