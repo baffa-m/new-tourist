8224c72213679f50939f1480494f0c2881ce176a
Ahmad Almustapha <ahmadalmustapha4@gmail.com>
2024-01-09 21:14:01 +0000

    destination and hotel views

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-
     Route::resource('hotel', HotelController::class);
     Route::resource('destination', DestinationController::class);
 
